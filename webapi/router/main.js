3711328a06ab20311e945296a38afe3e92654114
Tuise233 <2039203690@qq.com>
2022-05-02 10:47:48 +0800

    新增webapi: createDataId

--- webapi/router/main.js
+++ webapi/router/main.js
@@ -50,8 +50,42 @@ router.get('/search', (req, res) => {
         })
     } catch(err){
         console.log(err);
+    }  
+})
+
+router.get('/createDataId', (req, res) => {
+    try{
+        let dataid = req.query.dataid;
+        //判断是否存在dataid
+        executeReader(`SELECT * FROM user WHERE dataid='${dataid}'`, (reader, error) => {
+            if(error){
+                res.send({
+                    type: "error"
+                });
+            } else {
+                if(reader.length == 0){
+                    //创建dataid
+                    executeQuery(`INSERT INTO user (dataid) VALUE ('${dataid}')`, (error) => {
+                        if(error){
+                            res.send({
+                                type: "error"
+                            });
+                        } else {
+                            res.send({
+                                type: "success"
+                            });
+                        }
+                    })
+                } else {
+                    res.send({
+                        type: "exist"
+                    });
+                }
+            }
+        })
+    } catch(err){
+        console.log(err);
     }
-    
 })
 
 module.exports = router;
