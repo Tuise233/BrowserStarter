9a8149a2d3c568892b661d50d6e77c81524af7dd
Tuise233 <2039203690@qq.com>
2022-05-03 00:49:45 +0800

    新增webapi: isExistDataId 新增webapi: createFav 新增webapi: getFav

--- webapi/router/main.js
+++ webapi/router/main.js
@@ -88,4 +88,79 @@ router.get('/createDataId', (req, res) => {
     }
 })
 
+router.get('/isExistDataId', (req, res) => {
+    try{
+        let dataid = req.query.dataid;
+        executeReader(`SELECT * FROM user WHERE dataid='${dataid}'`, (reader, error) => {
+            if(error){
+                res.send({
+                    type: "error"
+                });
+            } else {
+                if(reader.length > 0){
+                    res.send({
+                        type: "exist"
+                    });
+                } else {
+                    res.send({
+                        type: "unexist"
+                    });
+                }
+            }
+        });
+    } catch(error){
+        console.log(error);
+    }
+})
+
+router.post('/createFav', (req, res) => {
+    try{
+        //console.log(req);
+        let color = req.body.color;
+        let name = req.body.name;
+        let url = req.body.url;
+        let pic = req.body.pic;
+        let dataid = req.body.dataid;
+        //存储
+        executeQuery(`INSERT INTO fav (dataid, name, url, pic, color) VALUES ('${dataid}', '${name}', '${url}', '${pic}', '${color}')`, (error) => {
+            if(error){
+                res.send({
+                    type: "error"
+                });
+            } else {
+                res.send({
+                    type: "success"
+                });
+            }
+        });
+    } catch(err){
+        console.log(err);
+    }
+});
+
+router.get('/getFav', (req, res) => {
+    let dataid = req.query.dataid;
+    executeReader(`SELECT * FROM fav WHERE dataid='${dataid}'`, (result, error) => {
+        if(error){
+            res.send({
+                type: "error"
+            });
+        } else {
+            let array = [];
+            for(let i = 0; i < result.length; i++){
+                array.push({
+                    name: result[i].name,
+                    url: result[i].url,
+                    pic: result[i].pic,
+                    color: result[i].color
+                });
+            }
+            res.send({
+                type: "success",
+                result: JSON.stringify(array)
+            });
+        }
+    })
+});
+
 module.exports = router;
