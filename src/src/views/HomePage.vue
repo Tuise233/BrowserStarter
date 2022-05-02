e7a36d26514bf91f00f82fcf026d54a2ce6674a0
Tuise233 <2039203690@qq.com>
2022-05-03 00:50:46 +0800

    新增快捷列表 新增添加快捷网址功能 新增独立用户数据同步 新增独立用户数据保存

--- src/src/views/HomePage.vue
+++ src/src/views/HomePage.vue
@@ -45,16 +45,18 @@
 
         <div class="favContainer">
             <div v-if="favList[0].length != 0" class="favRow">
-                <div class="favItem" v-for="(item,index) in favList[0]" :key="index">
-                    <img :src="item.pic">
-                    <span>{{item.name}}</span>
+                <div class="favItem favItem1" v-for="(item,index) in favList[0]" :key="index" v-on:click="onClickFav(item.url)" :id="index">
+                    <img v-if="item.pic != ''" :src="item.pic">
+                    <div v-if="item.pic == ''" class="prevText">{{item.name[0]}}</div>
+                    <span style="white-space: nowrap;">{{item.name}}</span>
                 </div>
             </div>
 
             <div v-if="favList[1].length != 0" class="favRow">
-                <div class="favItem" v-for="(item,index) in favList[1]" :key="index">
-                    <img :src="item.pic">
-                    <span>{{item.name}}</span>
+                <div class="favItem favItem2" v-for="(item,index) in favList[1]" :key="index" v-on:click="onClickFav(item.url)">
+                    <img v-if="item.pic != ''" :src="item.pic">
+                    <div v-if="item.pic == ''" class="prevText">{{item.name[0]}}</div>
+                    <span style="white-space: nowrap;">{{item.name}}</span>
                 </div>
             </div>
             <!-- <div class="favRow">
@@ -89,6 +91,38 @@
             <a>By: 拉布拉马</a>
         </div>
     </div>
+    <div class="addFavContainer">
+        <span style="font-size: 18px; margin-left: -240px;">添加链接</span>
+        
+        <img src="../assets/img/close.png" class="closeBtn" v-on:click="onClickAddClose">
+
+        <span>链接名称</span>
+        <input type="text" placeholder="请输入链接名称" v-model="prevName">
+
+        <span>链接地址</span>
+        <input type="text" placeholder="请输入链接地址" v-model="prevUrl">
+
+        <span>链接颜色</span>
+        <div style="width: 350px; height: 35px; display: flex; flex-direction: row; justify-content: center;">
+            <input class="disableScroll" style="width: 15%; margin: 0 6px;" type="number" placeholder="R值" v-model="prevColorR" @change="onColorChange">
+            <input class="disableScroll" style="width: 15%; margin: 0 6px;" type="number" placeholder="G值" v-model="prevColorG" @change="onColorChange">
+            <input class="disableScroll" style="width: 15%; margin: 0 6px;" type="number" placeholder="B值" v-model="prevColorB" @change="onColorChange">
+        </div>
+
+        <span>链接图标</span>
+        <button class="addIconBtn" v-on:click="onClickUploadIcon">上传</button>
+
+        <div class="addPreview">
+            <div class="previewItem">
+                <img class="prevImage" v-if="prevIconUrl != ''" :src="prevIconUrl" />
+                <div v-if="prevIconUrl == ''" class="prevText">{{prevName[0]}}</div>
+                <div v-if="prevName != null">{{prevName}}</div>
+            </div>
+        </div>
+
+        <button class="applyAddBtn" v-on:click="onClickApplyAdd">确定</button>
+    </div>
+    <input v-show="false" ref="fileRef" type="file" @change="onFileChange" />
     <div class="searchGlass" v-on:click="unfocusSearchEngine"></div>
     <div class="loadPage"></div>
   </div>
@@ -119,26 +153,25 @@ export default {
             
             dataid: "",
             favList: [
-                [
-                    {
-                    "name": "BiliBili",
-                    "pic": require("../assets/img/bilibili.png"),
-                    "url": "http://www.bilibili.com"
-                    }
-                ],
-                [
+                [],
+                []
+            ],
 
-                ]
-            ]
+            prevIconUrl: "",
+            prevIconFile: null,
+            prevName: "",
+            prevUrl: null,
+            prevColorR: null,
+            prevColorG: null,
+            prevColorB: null
         }
     },
 
     created() {
         document.title = "拉布拉马起始页";
 
-        axios({
-            method: "GET",
-            url: apiUrl + "/api/getSentence"
+        axios.get(apiUrl + "/api/getSentence", {
+
         }).then((res) => {
             this.dailySentence = res.data.text;
             this.dailySentenceEng = res.data.translate;
@@ -152,34 +185,110 @@ export default {
             let dataid = Math.random().toString(36).substr(2);
             localStorage.setItem("dataid", dataid);
             this.dataid = dataid;
-            axios({
-                method: "GET",
+            axios.get(apiUrl + "/api/createDataId", {
                 params: {
                     dataid: dataid
-                },
-                url: apiUrl + "/api/createDataId"
+                }
             });
         } else {
             this.dataid = localStorage.getItem("dataid");
+            //判断是否在数据库中
+            axios.get(apiUrl + "/api/isExistDataId", {
+                params: {
+                    dataid: this.dataid
+                }
+            }).then((res) => {
+                let type = res.data.type;
+                switch(type){
+                    case "unexist":{
+                        //创建数据
+                        axios.get(apiUrl + "/api/createDataId", {
+                            params: {
+                                dataid: this.dataid
+                            }
+                        });
+                        break;
+                    }
+                }
+            });
         }
 
-        //填充新增
-        var favCount = this.favList[0].length + this.favList[1].length;
-        if(favCount <= 6){
-            if(favCount < 6){
-                this.favList[0].push({
-                    name: "新增",
-                    pic: require("../assets/img/addon.png"),
-                    url: ""
-                })
-            } else {
-                this.favList[1].push({
-                    name: "新增",
-                    pic: require("../assets/img/addon.png"),
-                    url: ""
-                })
+        //获取列表 
+        axios.get(apiUrl + "/api/getFav", {
+            params: {
+                dataid: this.dataid
             }
-        }
+        }).then((res) => {
+            let array = JSON.parse(res.data.result);
+            console.log(array);
+            if(array.length <= 6){
+                if(array.length > 0){
+                    for(let i = 0; i < array.length; i++){
+                        this.favList[0].push({
+                            name: array[i].name,
+                            pic: array[i].pic,
+                            url: array[i].url,
+                            color: array[i].color
+                        });
+                    }
+                }
+            } else {
+                for(let i = 0; i < 6; i++){
+                    this.favList[0].push({
+                        name: array[i].name,
+                        pic: array[i].pic,
+                        url: array[i].url,
+                        color: array[i].color
+                    });
+                }
+
+                for(let i = 6; i < array.length; i++){
+                    this.favList[1].push({
+                        name: array[i].name,
+                        pic: array[i].pic,
+                        url: array[i].url,
+                        color: array[i].color
+                    });
+                }
+            }
+
+            //填充新增
+            var favCount = this.favList[0].length + this.favList[1].length;
+            if(favCount <= 6){
+                if(favCount < 6){
+                    this.favList[0].push({
+                        name: "添加",
+                        pic: require("../assets/img/addon.png"),
+                        url: "",
+                        color: "rgb(51, 51, 68)"
+                    });
+                } else {
+                    this.favList[1].push({
+                        name: "添加",
+                        pic: require("../assets/img/addon.png"),
+                        url: "",
+                        color: "rgb(51, 51, 68)"
+                    });
+                }
+            } else {
+                if(favCount != 12){
+                    this.favList[1].push({
+                        name: "添加",
+                        pic: require("../assets/img/addon.png"),
+                        url: "",
+                        color: "rgb(51, 51, 68)"
+                    })
+                }
+            }
+
+            setTimeout(() => {
+                this.setFavData();
+                setTimeout(() => {
+                    var fav = this.getClass("favContainer", 0);
+                    fav.style.opacity = "1";
+                }, 500);
+            }, 500);
+        })
     },
 
     mounted() {
@@ -202,6 +311,19 @@ export default {
     },
 
     methods: {
+        setFavData: function(){
+            //设置颜色
+            var fav1 = document.getElementsByClassName("favItem1");
+            for(let i = 0; i < fav1.length; i++){
+                fav1[i].children[0].style.backgroundColor = this.favList[0][i].color;
+            }
+
+            var fav2 = document.getElementsByClassName("favItem2");
+            for(let i = 0; i < fav2.length; i++){
+                fav2[i].children[1].style.backgroundColor = this.favList[1][i].color;
+            }
+        },
+
         isMobile: function(){
             let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
             return flag;
@@ -358,6 +480,178 @@ export default {
             document.execCommand("Copy");
             document.body.removeChild(input);
             alert("成功复制数据ID，请及时保存");
+        },
+
+        onClickFav: function(url){
+            if(url == ""){
+                //打开新增数据表
+                var addPanel = this.getClass("addFavContainer", 0);
+                var glass = this.getClass("searchGlass", 0);
+                glass.style.zIndex = "98";
+                glass.style.opacity = "1";
+                addPanel.style.zIndex = "99";
+                addPanel.style.opacity = "1";
+            } else {
+                window.open(url, "_self");
+            }
+        },
+
+        onClickAddClose: function(){
+            var addPanel = this.getClass("addFavContainer", 0);
+            var glass = this.getClass("searchGlass", 0);
+            glass.style.zIndex = "-1";
+            glass.style.opacity = "0";
+            addPanel.style.zIndex = "-1";
+            addPanel.style.opacity = "0";
+        },
+
+        onClickUploadIcon: function(){
+            this.$refs.fileRef.dispatchEvent(new MouseEvent('click'));
+        },
+
+        onClickApplyAdd: function(){
+            /*
+            prevIconUrl: null,
+            prevIconFile: null,
+            prevName: null,
+            prevUrl: null,
+            prevColorR: null,
+            prevColorG: null,
+            prevColorB: null
+            */
+            //rgb(51, 51, 68)
+            let colorR = null;
+            let colorG = null;
+            let colorB = null;
+            if(this.prevColorR == null && this.colorG == null && this.colorB == null){
+                colorR = 51;
+                colorG = 51;
+                colorB = 68;
+            } else {
+                colorR = this.prevColorR == null ? 0 : this.prevColorR;
+                colorG = this.prevColorG == null ? 0 : this.prevColorG;
+                colorB = this.prevColorB == null ? 0 : this.prevColorB;
+            }
+            let iconUrl = this.prevIconUrl;
+            let name = this.prevName;
+            let url = this.prevUrl;
+            let color = `rgb(${colorR},${colorG},${colorB})`;
+            let dataid = localStorage.getItem("dataid");
+            
+            if(name == ""){
+                alert("请填写链接名称");
+            } else if(url == null){
+                alert("请填写链接地址");
+            } else {
+                //保存链接
+                //console.log(`${iconUrl} ${name} ${url} ${color} ${dataid}`);
+                const datas = new URLSearchParams();
+                datas.append("dataid", dataid);
+                datas.append("color", color);
+                datas.append("name", name);
+                datas.append("url", url);
+                datas.append("pic", iconUrl);
+                axios.post(apiUrl + "/api/createFav", datas).then((res) => {
+                    var addPanel = this.getClass("addFavContainer", 0);
+                    var glass = this.getClass("searchGlass", 0);
+                    glass.style.zIndex = "-1";
+                    glass.style.opacity = "0";
+                    addPanel.style.zIndex = "-1";
+                    addPanel.style.opacity = "0";
+                });
+                //添加到列表中
+                if(this.favList[0].length <= 6){
+                    if(this.favList[0].length <= 5){
+                        this.favList[0].splice(this.favList[0].length - 1, 1);
+                        this.favList[0].push({
+                            name: name,
+                            pic: iconUrl,
+                            url: url,
+                            color: color
+                        });
+                        this.favList[0].push({
+                            name: "添加",
+                            pic: require("../assets/img/addon.png"),
+                            url: "",
+                            color: "rgb(51, 51, 68)"
+                        });
+                    } else if(this.favList[0].length == 6){
+                        this.favList[0].splice(5, 1);
+                        this.favList[0].push({
+                            name: name,
+                            pic: iconUrl,
+                            url: url,
+                            color: color
+                        });
+                        this.favList[1].push({
+                            name: "添加",
+                            pic: require("../assets/img/addon.png"),
+                            url: "",
+                            color: "rgb(51, 51, 68)"
+                        });
+                    }
+                } else {
+                    if(this.favList[1].length < 6){
+                        if(this.favList[1].length <= 5){
+                            this.favList[0].splice(this.favList[1].length - 1, 1);
+                            this.favList[1].push({
+                                name: name,
+                                pic: iconUrl,
+                                url: url,
+                                color: color
+                            });
+                            this.favList[1].push({
+                                name: "添加",
+                                pic: require("../assets/img/addon.png"),
+                                url: "",
+                                color: "rgb(51, 51, 68)"
+                            });
+                        } else {
+                            this.favList[1].splice(5, 1);
+                            this.favList[1].push({
+                                name: name,
+                                pic: iconUrl,
+                                url: url,
+                                color: color
+                            });
+                        }
+                    }
+                }
+
+                //重新设置颜色
+                setTimeout(() => {
+                    this.setFavData();
+                }, 500);
+            }
+        },
+
+        onFileChange: function(obj){
+            let _this = this;
+            var files = obj.target.files[0];
+            if(!obj || !window.FileReader) return;
+            let reader = new FileReader();
+            reader.readAsDataURL(files);
+            reader.onload = () => {
+                _this.prevIconUrl = reader.result;
+            };
+        },
+
+        onColorChange: function(){
+            var image = this.getClass("prevImage", 0);
+            var text = this.getClass("prevText", 0);
+            if(image != null){
+                if(this.prevColorR != null && this.prevColorG != null || this.prevColorB != null){
+                    image.style.backgroundColor = `rgb(${this.prevColorR},${this.prevColorG},${this.prevColorB})`;
+                } else {
+                    image.style.backgroundColor = "";
+                }
+            } else if(text != null){
+                if(this.prevColorR != null && this.prevColorG != null || this.prevColorB != null){
+                    text.style.backgroundColor = `rgb(${this.prevColorR},${this.prevColorG},${this.prevColorB})`;
+                } else {
+                    text.style.backgroundColor = "";
+                }
+            }
         }
     },
 }
